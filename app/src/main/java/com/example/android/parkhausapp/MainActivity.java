f9a7d104c8eac52c9735ac1b34daa7b3cbda9e11
kobrons <heiko230@gmx.de>
2019-02-02 23:22:47 +0100

    Margins added. Code aufgeräumt. Farbe für halbe Stockwerke hinzugefügt

--- app/src/main/java/com/example/android/parkhausapp/MainActivity.java
+++ app/src/main/java/com/example/android/parkhausapp/MainActivity.java
@@ -17,18 +17,31 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout rootViewFull = (LinearLayout) findViewById(R.id.LayoutFullNumbers);
         LinearLayout rootViewHalf = (LinearLayout) findViewById(R.id.LayoutHalfNumbers);
 
+
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
 
         for (int i = 8; i>=0; i--){
+            //-- Erstellen d. Textviews --//
             TextView stageViewFull = new TextView(this);
             TextView stageViewHalf = new TextView(this);
+
             if (i>0)
                 stageViewFull.setText("" + i);
             else
-                stageViewFull.setText("");
+                stageViewFull.setText("E");
             stageViewHalf.setText(i + ".5");
+
+            //-- Hintergrundfarben --//
             stageViewFull.setBackgroundResource(R.color.colorPrimary);
-            stageViewFull.setPadding(25, 25, 25, 25);
+            stageViewHalf.setBackgroundResource(R.color.colorAccent);
+
+            //-- Padding --//
+            stageViewFull.setPadding(50, 40, 50, 40);
+            stageViewHalf.setPadding(50, 40, 50, 40);
+
+            //-- Margin --//
+            params.setMargins(50, 10, 50, 0);
+
 
 
             //-- Casten der Views --//
